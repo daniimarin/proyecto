510c3a80d9101ab452420834bd9651995f6b1648
Steff0818 <steffanny08@hotmail.com>
2024-06-18 19:34:13 -0600

    Elimine una parte que estaba dos veces

--- pryecto dani y steff2.R
+++ pryecto dani y steff2.R
@@ -72,91 +72,7 @@ server <- function(input, output) {
       Orden_Pais
     })
   })
-  # Server de la aplicación
-  server <- function(input, output) {
-    
-    datos_filtrados <- reactive({
-      Votos1 <- votes %>%
-        filter(vote %in% input$checkbox_votos) %>%
-        mutate(year = 1945 + session,
-               country = countrycode(ccode, "cown", "country.name")) %>%
-        mutate(country = recode(country,
-                                'United States of America' = 'United States',
-                                'United Kingdom of Great Britain and Northern Ireland' = 'United Kingdom'))
-      Votos1
-    })
-    
-    output$tabla_votos <- renderTable({
-      votos_filtrados <- datos_filtrados()
-      
-      votos_resumen <- votos_filtrados %>%
-        group_by(vote) %>%
-        summarise(
-          Total = n(),
-          Porcentaje = (Total / nrow(votos_filtrados)) * 100
-        )
-      
-      votos_resumen
-    })
-    
-    observeEvent(input$btn_pais, {
-      Votos1 <- datos_filtrados()
-      Orden_Pais <- Votos1 %>%
-        filter(vote == 1) %>%
-        group_by(country) %>%
-        summarise(
-          Total = n(),
-          Porcentaje = (Total / nrow(Votos1)) * 100
-        ) %>%
-        arrange(desc(Porcentaje))
-      
-      output$tabla_pais <- renderTable({
-        Orden_Pais
-      })
-    })
-    # Server de la aplicación
-    server <- function(input, output) {
-      
-      datos_filtrados <- reactive({
-        Votos1 <- votes %>%
-          filter(vote %in% input$checkbox_votos) %>%
-          mutate(year = 1945 + session,
-                 country = countrycode(ccode, "cown", "country.name")) %>%
-          mutate(country = recode(country,
-                                  'United States of America' = 'United States',
-                                  'United Kingdom of Great Britain and Northern Ireland' = 'United Kingdom'))
-        Votos1
-      })
-      
-      output$tabla_votos <- renderTable({
-        votos_filtrados <- datos_filtrados()
-        
-        votos_resumen <- votos_filtrados %>%
-          group_by(vote) %>%
-          summarise(
-            Total = n(),
-            Porcentaje = (Total / nrow(votos_filtrados)) * 100
-          )
-        
-        votos_resumen
-      })
-      
-      observeEvent(input$btn_pais, {
-        Votos1 <- datos_filtrados()
-        Orden_Pais <- Votos1 %>%
-          filter(vote == 1) %>%
-          group_by(country) %>%
-          summarise(
-            Total = n(),
-            Porcentaje = (Total / nrow(Votos1)) * 100
-          ) %>%
-          arrange(desc(Porcentaje))
-        
-        output$tabla_pais <- renderTable({
-          Orden_Pais
-        })
-      })
-      
+  
       output$grafico_votos <- renderPlot({
         Votos1 <- datos_filtrados()
         
@@ -219,6 +135,8 @@ server <- function(input, output) {
       })
     }
     
+    # Ejecutar la aplicación Shiny
+shinyApp(ui = ui, server=server)
     
           
           
